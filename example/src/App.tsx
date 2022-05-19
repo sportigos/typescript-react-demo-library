8f86a1427a81cf9ec6838717b3f3587bf262ce05
sportigos <sportigo1120@gmail.com>
2022-05-19 05:43:07 -0500

    initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Widget } from "@jannelei/typescript-react-test";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Widget />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type ComponentProps = {};
+
+const Widget: React.FC<ComponentProps> = (): JSX.Element => {
+  return <>Widget built with NPM Package</>;
+};
+
+export default Widget;
